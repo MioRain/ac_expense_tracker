ffb9a84b6d08ffc9683000635772d969ebd11b00
MioRain <ericzero1593@hotmail.com>
2022-06-24 16:11:49 +0800

    feat: add item & user schema

---
+++ models/item.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const itemSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Item', itemSchema)
