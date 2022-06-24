b9c86dc5317ae2e2c7ab76e1142cf532114b11a5
MioRain <ericzero1593@hotmail.com>
2022-06-25 04:59:21 +0800

    feat: add create function

--- routes/modules/items.js
+++ routes/modules/items.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const Item = require('../../models/item')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
   res.render('new')
 })
 
-router.post('/', (req, res) => {
-  console.log(req.body)
+router.post('/', async (req, res) => {
+  try {
+    const item = req.body
+    await Item.create(item)
+    res.redirect('/')
+  }
+  catch (err) {
+    console.log('catch', err)
+  }
 })
 
 module.exports = router
