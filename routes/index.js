8ec21cfd601bfdc67dd9eeddd4da987ecc4384f5
MioRain <ericzero1593@hotmail.com>
2022-06-25 17:33:38 +0800

    feat: add user router & login page

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const home = require('./modules/home')
 const records = require('./modules/records')
 const categories = require('./modules/categories')
+const users = require('./modules/users')
 
 router.use('/records', records)
 router.use('/categories', categories)
+router.use('/users', users)
 router.use('/', home)
 
 module.exports = router
